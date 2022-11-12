1ed920acbcc3794593fc1dbc68f7fb3067b17a27
Alex <alex95.taylor@gmail.com>
2022-11-12 15:15:46 -0500

    Delete files

--- prompt2/backend/backend/boredAPI/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BoredAPI
-
-class BoredAPIAdmin(admin.ModelAdmin):
-    list_display = ('activity', 'type', 'participants')
-
-# Register your models here.
-admin.site.register(BoredAPI, BoredAPIAdmin)
