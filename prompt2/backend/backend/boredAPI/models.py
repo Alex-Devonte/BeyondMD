cd629736aaed9f2d1311edb756507e8b4e446609
Alex <alex95.taylor@gmail.com>
2022-11-12 12:58:10 -0500

    Create borderAPI model

--- prompt2/backend/backend/boredAPI/models.py
+++ prompt2/backend/backend/boredAPI/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class BoredAPI(models.Model):
+    #tuple for activity types
+    # TUPLE_EXAMPLE = (
+    #     ('value to set on model','human-readable name')
+    # )
+    ACTIVITY_TYPE = [
+        ('education', 'Education'),
+        ('recreational', 'Recreational'),
+        ('social', 'Social'),
+        ('diy', 'DIY'),
+        ('charity', 'Charity'),
+        ('cooking', 'Cooking'),
+        ('relaxation', 'Relaxation'),
+        ('music', 'Music'),
+        ('busywork', 'Busywork')
+    ]
+
+    activity = models.CharField(max_length=120)
+    type = models.CharField(max_length=20, choices=ACTIVITY_TYPE)
+    participants = models.CharField(max_length=2)
