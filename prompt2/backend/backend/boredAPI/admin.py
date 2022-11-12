1c22286d468842a0b52d91c5eb928500ab61d45c
Alex <alex95.taylor@gmail.com>
2022-11-12 13:46:20 -0500

    Set up initial admin interface

--- prompt2/backend/backend/boredAPI/admin.py
+++ prompt2/backend/backend/boredAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import BoredAPI
+
+class BoredAPIAdmin(admin.ModelAdmin):
+    list_display = ('activity', 'type', 'participants')
 
 # Register your models here.
+admin.site.register(BoredAPI, BoredAPIAdmin)
