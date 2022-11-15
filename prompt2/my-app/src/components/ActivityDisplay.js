0539f60046fdb6f64f5926bea01715f8ea54fa8c
Alex <alex95.taylor@gmail.com>
2022-11-15 18:35:08 -0500

    Added the 'type' and 'participants' field to display

--- prompt2/my-app/src/components/ActivityDisplay.js
+++ prompt2/my-app/src/components/ActivityDisplay.js
@@ -5,25 +5,30 @@ import Button from '@mui/material/Button';
 
 function ActivityDisplay() {
     //Declare state variable 'activity'
-    const [activity, setActivity] = useState("");
+    const [activity, setActivity] = useState({});
 
     //Make call to api to set activity variable
     const getActivity = () => {
         axios.get("https://www.boredapi.com/api/activity").then(
             (response) => {
-                //console.log(response);
-                setActivity(response.data.activity);
+                setActivity({activity: response.data.activity, type: response.data.type, participants: response.data.participants});
             }
         );
     }
+    //Todo: find way to display on click
     return (
         <div id='activity-container'>
             <h2>Feeling bored?</h2>
             <h3>Click the button and find something to do!</h3>
             <Button variant="contained" size="large" id="generate-btn" onClick={() => {getActivity()}}>Generate activity</Button>
-            <div id="activity">
-                { activity }
-            </div>
+            {/*Show element only if activity.type isn't empty*/}
+            {activity.type 
+            ? <div id="activity">
+                Activity: {activity.activity}<br/>
+                Type: {activity.type}<br/>
+                Participants: {activity.participants}
+              </div>
+            : null}
         </div>
     )
 }
