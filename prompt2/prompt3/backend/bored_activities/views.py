fbd42433c49de6bcec2418836b61f990204f5505
Alex <alex95.taylor@gmail.com>
2022-11-14 21:22:08 -0500

    Create bored_activities view

--- prompt2/prompt3/backend/bored_activities/views.py
+++ prompt2/prompt3/backend/bored_activities/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import Bored_Activities_Serializer
+from .models import Bored_Activities
 
 # Create your views here.
+#Viewsets provides the implementation CRUD operations
+
+class Bored_Activities_View(viewsets.ModelViewSet):
+    #Used for validation and deserializing input
+    serializer_class = Bored_Activities_Serializer
+    #Describes all objects in the database table
+    queryset = Bored_Activities.objects.all()
