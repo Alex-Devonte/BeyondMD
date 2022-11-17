8be03af5fe71ff272341fce942a4134d91b183cc
Alex <alex95.taylor@gmail.com>
2022-11-17 02:51:07 -0500

    Add component that displays response in table

---
+++ prompt2/my-app/src/components/Activities.js
@@ -0,0 +1,48 @@
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import axios from "axios";
+import {useEffect, useState } from "react";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function Activities() {
+    const [activities, setActivities] = useState([{}]);
+
+    //Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        //Pull data from database after update
+        GetActivities();
+    });
+
+    //Get all activities in database
+    function GetActivities() {
+        axios.get("http://localhost:8000/api/bored_activities/")
+            .then((response) => {
+                setActivities(response.data);
+            });
+    }
+
+    return (
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Activity</TableCell>
+                    <TableCell>Type</TableCell>
+                    <TableCell>Participants</TableCell>
+                    <TableCell>Modify</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {activities.map(({id, activity, type, participants}) => (
+                    <TableRow key={id}>
+                        <TableCell>{activity}</TableCell>
+                        <TableCell>{type}</TableCell>
+                        <TableCell>{participants}</TableCell>
+                        <TableCell><EditIcon/><DeleteIcon/></TableCell>
+                    </TableRow>
+                    ))}
+            </TableBody>
+        </Table>
+    );
+}
+
+export default Activities;
