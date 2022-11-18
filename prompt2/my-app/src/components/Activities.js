2e5eb7cd078dfd5f8b02ec29978f69b853e2f665
Alex <alex95.taylor@gmail.com>
2022-11-17 21:48:38 -0500

    Add delete activity functionality

--- prompt2/my-app/src/components/Activities.js
+++ prompt2/my-app/src/components/Activities.js
@@ -1,4 +1,4 @@
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import axios from "axios";
 import {useEffect, useState } from "react";
 import EditIcon from '@mui/icons-material/Edit';
@@ -7,12 +7,14 @@ import './Activities.css';
 
 function Activities() {
     const [activities, setActivities] = useState([{}]);
+    const [open, setOpen] = useState(false);
+    const [_id, setId] = useState(null);
 
     //Similar to componentDidMount and componentDidUpdate:
     useEffect(() => {
         //Pull data from database after update
         GetActivities();
-    });
+    }, [activities.length]); //Only update if length of array changes
 
     //Get all activities in database
     function GetActivities() {
@@ -20,6 +22,32 @@ function Activities() {
             .then((response) => {
                 setActivities(response.data);
             });
+    }
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    //Open dialog confirming deletion before making api call
+    function handleDelete(id) {
+        handleOpen();
+        setId(id);
+    }
+
+    //Delete activity by id
+    function deleteActivity(id) {
+        handleClose();
+        axios.delete(`http://localhost:8000/api/bored_activities/${id}`)
+        .then((response) => {
+            alert("Activity successfully deleted.");
+
+            //Get list after deletion
+            GetActivities();
+        });
     }
 
     return (
@@ -40,7 +68,16 @@ function Activities() {
                         <TableCell>{participants}</TableCell>
                         <TableCell>
                             <EditIcon   className="edit-icon"   sx={{color: '#f3b24a'}}/>
-                            <DeleteIcon className="delete-icon" sx={{color: '#ca0000'}}/>
+                            <DeleteIcon className="delete-icon" onClick={() => handleDelete(id)} sx={{color: '#ca0000'}}/>
+                            <Dialog open={open} onClose={handleClose}>
+                                <DialogContent>
+                                    Are you sure you want to delete this activity?
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={() => deleteActivity(_id)}>Yes</Button>
+                                    <Button onClick={handleClose}>No</Button>
+                                </DialogActions>
+                            </Dialog>
                         </TableCell>
                     </TableRow>
                     ))}
